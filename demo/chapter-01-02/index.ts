b154d7575da5a792114356a2a825e61ce40dff08
chenshenhai <cshenhai@gmail.com>
2018-12-16 17:12:32 +0800

    feat: init

---
+++ demo/chapter-01-02/index.ts
@@ -0,0 +1,8 @@
+// module
+import { hello } from './lib/hello.ts';
+import world from './lib/world.ts';
+
+const str1 = hello();
+const str2 = world.world();
+
+console.log(`${str1} ${str2}`);
---
+++ demo/chapter-01-02/lib/hello.ts
@@ -0,0 +1,3 @@
+export const hello = function():string {
+  return 'hello'
+}
