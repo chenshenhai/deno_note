69cab0cb3f5338f0426697a8fb1628387cf1eb73
chenshenhai <cshenhai@gmail.com>
2018-12-16 17:32:10 +0800

    feat: add tslint config

--- demo/module/index.ts
+++ demo/module/index.ts
@@ -1,6 +1,6 @@
 // module
-import { hello } from './lib/hello.ts';
-import world from './lib/world.ts';
+import { hello } from './lib/hello';
+import world from './lib/world';
 
 const str1 = hello();
 const str2 = world.world();
