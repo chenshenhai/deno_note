bbcb2c75fe05158ed63c4f03d5e4f9351b547b76
chenshenhai <cshenhai@gmail.com>
2018-12-16 17:54:21 +0800

    feat: update tslint config

--- demo/hello/index.ts
+++ demo/hello/index.ts
@@ -1,2 +1,2 @@
 // hello world
-console.log('Hello World');
+console.log("Hello World");
--- demo/module/lib/hello.ts
+++ demo/module/lib/hello.ts
@@ -1,3 +1,3 @@
-export const hello = function():string {
-  return 'hello'
-}
+export const hello = function(): string {
+  return "hello";
+};
