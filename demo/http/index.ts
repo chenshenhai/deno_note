d24a68cfc665e8f066daf0049cfd9a6faebc78f6
chenshenhai <cshenhai@gmail.com>
2018-12-27 23:52:47 +0800

    feat: update demo

--- demo/http/index.ts
+++ demo/http/index.ts
@@ -1,35 +1,40 @@
 
-import { args, listen, copy, Conn } from "deno";
+import { args, listen, Conn } from "deno";
 
 const addr = args[1] || "127.0.0.1:3001";
 const CRLF = "\r\n";
 const encoder = new TextEncoder();
-const body = encoder.encode(JSON.stringify({
-  hello: "world"
-}));
 
-const lines = [
-  "HTTP/1.1 ",
-  `Content-Length: ${body.length}`,
-  `${CRLF}`
-].join(CRLF);
-const envelope = encoder.encode(lines);
-const data = new Uint8Array(envelope.length + (body ? body.length : 0));
-data.set(envelope, 0);
-if (body) {
-  data.set(body, envelope.length);
+function getHttpContext (): Uint8Array {
+  // const bodyStr = JSON.stringify({hello: "world"});
+  const bodyStr = "<h1>hello world</h1>";
+  const body = encoder.encode(bodyStr);
+  const headers = [
+    "HTTP/1.1 ",
+    `Content-Length: ${body.length}`,
+    `${CRLF}`
+  ].join(CRLF);
+  const ctx = encoder.encode(headers);
+  const data = new Uint8Array(ctx.length + (body ? body.length : 0));
+  data.set(ctx, 0);
+  if (body) {
+    data.set(body, ctx.length);
+  }
+  return data;
 }
 
-const server = async function(addr: string) {
+async function server(addr: string) {
   const listener = listen("tcp", addr);
+  console.log("listening on", addr);
+  const ctx = getHttpContext();
   async function loop(conn: Conn) {
-    await conn.write(data);
+    await conn.write(ctx);
     conn.close();
   }
   while (true) {
     const connection = await listener.accept();
     loop(connection);
   }
-};
+}
 
 server(addr);
