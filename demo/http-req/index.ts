20a4f36a3d0a8e4aa9f26993fae4ec0741459ce6
chenshenhai <cshenhai@gmail.com>
2018-12-30 12:15:58 +0800

    feat: update demo

--- demo/http-req/index.ts
+++ demo/http-req/index.ts
@@ -30,7 +30,24 @@ async function getReq(conn: Conn): Promise<string> {
   const headerList = headers.split(CRLF);
   headerList.forEach(function(item, i) {
     if (i === 0) {
-      headersObj["method"] = item;
+      // headersObj["method"] = item;
+      if (typeof item === "string") {
+        // example "GET /index/html?a=1 HTTP/1.1";
+        const regMatch = /([A-Z]{1,}){1,}\s(.*)\s(.*)/;
+        const strList : object = item.match(regMatch);
+        const method : string = strList[1] || "";
+        const href : string = strList[2] || "";
+        const protocol : string = strList[3] || "";
+        
+        const pathname : string = href.split("?")[0] || "";
+        const search : string = href.split("?")[1] || "";
+        
+        headersObj["method"] = method;
+        headersObj["protocol"] = protocol;
+        headersObj["href"] = href;
+        headersObj["pathname"] = pathname;
+        headersObj["search"] = search;
+      }
     } else {
       if (typeof item === "string" && item.length > 0) {
         const itemList = item.split(":");
