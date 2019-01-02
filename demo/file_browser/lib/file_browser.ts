66a5d4948df00a91b7efa063994fe0cc4c03dfac
chenshenhai <cshenhai@gmail.com>
2019-01-02 23:02:05 +0800

    feat: update demo

--- demo/file_browser/lib/file_browser.ts
+++ demo/file_browser/lib/file_browser.ts
@@ -1,7 +1,18 @@
+import {readDirSync, readFileSync} from 'deno';
+
+function renderDir() {
+  // TODO
+}
+
+function renderFile() {
+  // TODO
+}
+
 function fileBrowser(baseDir: string): Function {
   return async function(ctx) {
     const {req, res} = ctx;
-    res.setBody(`${baseDir}`);
+    const stat = readDirSync(baseDir);
+    res.setBody(`${JSON.stringify(stat)}`);
   };
 }
 
