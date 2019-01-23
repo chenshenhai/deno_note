49b0f4e36bfb74ff53a6b0baf631be60b44d95cc
chenshenhai <cshenhai@gmail.com>
2019-01-24 00:48:09 +0800

    feat: update demo

--- demo/chapter-05-01/write.ts
+++ demo/chapter-05-01/write.ts
@@ -4,7 +4,7 @@ const encoder = new TextEncoder();
 
 async function main(): Promise<void> {
   const data = encoder.encode("this is writing result!");
-  const text = await writeFileSync("./assets/result.txt", data);
+  await writeFileSync("./assets/result.txt", data);
 }
 
 main();
