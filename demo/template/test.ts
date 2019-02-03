2365c6cbe75ceab50b9b1557152fbf611ae3bd43
chenshenhai <cshenhai@gmail.com>
2019-02-03 14:18:38 +0800

    feat: update demo testing

---
+++ demo/template/test.ts
@@ -0,0 +1,45 @@
+import { test, assert, runTests } from "https://deno.land/x/testing/mod.ts";
+import { compileTemplate } from "./mod.ts";
+
+test(function testCompileTemplate() {
+  const tpl = `
+  <div>
+  #if( datalist && datalist.length > 0 )
+    <ul>
+    #foreach(datalist).indexAs(i)
+      <li>{{i}}:{{datalist[i]}}</li>
+    #/foreach
+    </ul>
+  #elseif( showOthers === true )
+    <p> others </p>
+  #else
+    <p> default </p>
+  #/if
+
+  #if( datajson)
+    <ul>
+    #foreach(datajson).keyAs(k)
+      <li>{{k}}:{{datajson[k]}}</li>
+    #/foreach
+    </ul>
+  #/if
+  </div>`;
+  const data = {
+    title: "helloworld",
+    text: "hellopage",
+    isShowDataList: true,
+    datalist: [
+      "item1", "item2", "item3"
+    ],
+    datajson: {
+      "key1": "val1",
+      "key2": "val2"
+    }
+  };
+  const html = compileTemplate(tpl, data);
+  const expectedResult = `  <div>      <ul>          <li>0:item1</li>          <li>1:item2</li>          <li>2:item3</li>        </ul>        <ul>          <li>key1:val1</li>          <li>key2:val2</li>        </ul>    </div>`
+  assert.equal(1, 1);
+  assert.equal(expectedResult, html);
+});
+
+runTests();
