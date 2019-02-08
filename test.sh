9bf575ee65c1cc0c900f2ca98b5a0e754345f455
chenshenhai <cshenhai@gmail.com>
2019-02-08 13:11:07 +0800

    feat: update testing shell

--- test.sh
+++ test.sh
@@ -1,2 +1,15 @@
 
+# deno --allow-run --allow-net test.ts
+
+cd demo/buffer_reader/
+deno --allow-run --allow-net test.ts
+
+cd ../request/
+deno --allow-run --allow-net test.ts
+
+cd ../response/
+deno --allow-run --allow-net test.ts
+
+
+cd ../server/
 deno --allow-run --allow-net test.ts
