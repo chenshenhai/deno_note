e1022941083d06ed402110a902b0416dfa668ffb
chenshenhai <cshenhai@gmail.com>
2019-02-10 12:46:39 +0800

    feat: update buffer_reader/example

---
+++ demo/buffer_reader/example_01.ts
@@ -0,0 +1,17 @@
+import { Reader, Buffer } from "deno";
+
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
+
+const str = `hello\r\nworld\r\n!\r\n`;
+const stream = encoder.encode(str);
+const reader = new Buffer(stream);
+const chunk: Uint8Array = new Uint8Array(8);
+
+async function main() {
+  const result = await reader.read(chunk);
+  console.log(result) // export:  { nread: 8, eof: false }
+  console.log(decoder.decode(chunk)); // export: hello\r\nw
+}
+
+main();
