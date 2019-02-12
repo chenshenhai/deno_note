1f91dc6132677bb6a202da67ed94c5e4a4335e37
chenshenhai <cshenhai@gmail.com>
2019-02-13 00:22:16 +0800

    feat: update testing

--- demo/web_static/test.ts
+++ demo/web_static/test.ts
@@ -9,7 +9,7 @@ let httpServer;
 
 async function startHTTPServer() {
   httpServer = run({
-    args: ["deno", "--allow-net", "./test_server.ts", ".", "--cors"],
+    args: ["deno", "--allow-net", "--allow-read", "./test_server.ts", ".", "--cors"],
     stdout: "piped"
   });
   const buffer = httpServer.stdout;
