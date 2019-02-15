8a931e08a1196c876ebd90ba334b0f7dd47fad9a
chenshenhai <cshenhai@gmail.com>
2019-02-16 01:44:49 +0800

    feat: update testing

--- test.ts
+++ test.ts
@@ -19,11 +19,11 @@ const testUnitRunList = [
     cwd: "./demo/response/",
     stdout: "piped"
   },
-  // {
-  //   args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
-  //   cwd: "./demo/server/",
-  //   stdout: "piped"
-  // },
+  {
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    cwd: "./demo/server/",
+    stdout: "piped"
+  },
   // {
   //   args: ["deno", "--recompile", "test.ts", ".", "--cors"],
   //   cwd: "./demo/template/",
