97dc02626759d5020017433b4bfbdc56ab583e34
chenshenhai <cshenhai@gmail.com>
2019-02-16 01:31:26 +0800

    feat: update testing and readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # deno进阶开发笔记
 
 [![Build Status](https://travis-ci.com/chenshenhai/deno_note.svg?token=XYNG2F1URZ4nW1TzoJNC&branch=master)](https://travis-ci.com/chenshenhai/deno_note)
+
+<hr/>
+
+<img  width="240"  src="https://user-images.githubusercontent.com/8216630/52873226-771a3e80-3189-11e9-9bf9-59de7091dbfa.png">
+
--- test.ts
+++ test.ts
@@ -5,42 +5,42 @@ const decoder = new TextDecoder();
 
 const testUnitRunList = [
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/buffer_reader/",
     stdout: "piped"
   },
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/request/",
     stdout: "piped"
   },
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/response/",
     stdout: "piped"
   },
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/server/",
     stdout: "piped"
   },
   {
-    args: ["deno", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "test.ts", ".", "--cors"],
     cwd: "./demo/template/",
     stdout: "piped"
   },
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/web/",
     stdout: "piped"
   },
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/web_router/",
     stdout: "piped"
   },
   {
-    args: ["deno", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/web_static/",
     stdout: "piped"
   },
