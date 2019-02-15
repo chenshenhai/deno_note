c5ea9c710498092fad457e2e4e9211f604e11abd
chenshenhai <cshenhai@gmail.com>
2019-02-16 01:38:59 +0800

    feat: update testing

--- test.ts
+++ test.ts
@@ -9,41 +9,41 @@ const testUnitRunList = [
     cwd: "./demo/buffer_reader/",
     stdout: "piped"
   },
-  {
-    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
-    cwd: "./demo/request/",
-    stdout: "piped"
-  },
+  // {
+  //   args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+  //   cwd: "./demo/request/",
+  //   stdout: "piped"
+  // },
   {
     args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
     cwd: "./demo/response/",
     stdout: "piped"
   },
-  {
-    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
-    cwd: "./demo/server/",
-    stdout: "piped"
-  },
-  {
-    args: ["deno", "--recompile", "test.ts", ".", "--cors"],
-    cwd: "./demo/template/",
-    stdout: "piped"
-  },
-  {
-    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
-    cwd: "./demo/web/",
-    stdout: "piped"
-  },
-  {
-    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
-    cwd: "./demo/web_router/",
-    stdout: "piped"
-  },
-  {
-    args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
-    cwd: "./demo/web_static/",
-    stdout: "piped"
-  },
+  // {
+  //   args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+  //   cwd: "./demo/server/",
+  //   stdout: "piped"
+  // },
+  // {
+  //   args: ["deno", "--recompile", "test.ts", ".", "--cors"],
+  //   cwd: "./demo/template/",
+  //   stdout: "piped"
+  // },
+  // {
+  //   args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+  //   cwd: "./demo/web/",
+  //   stdout: "piped"
+  // },
+  // {
+  //   args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+  //   cwd: "./demo/web_router/",
+  //   stdout: "piped"
+  // },
+  // {
+  //   args: ["deno", "--recompile", "--allow-run", "--allow-net", "test.ts", ".", "--cors"],
+  //   cwd: "./demo/web_static/",
+  //   stdout: "piped"
+  // },
 ]
 
 async function runUnitTest(opts: RunOptions): Promise<string> {
