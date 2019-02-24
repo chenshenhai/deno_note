b04fb20de5dcb1aae6ed2c68f0df41ef16e69b63
chenshenhai <cshenhai@gmail.com>
2019-02-24 14:13:51 +0800

    docs: update chapter_03/01

--- demo/window/api.ts
+++ demo/window/api.ts
@@ -5,4 +5,4 @@ const apiList = Object.keys(window)
 // })
 
 console.log(`Supported global API [count: ${apiList.length}]: \r\n`);
-console.log(`${apiList.join(',')} \r\n`);
+console.log(`${apiList.join(', ')} \r\n`);
