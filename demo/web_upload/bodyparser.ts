888b30e3d753f16d2a77ec858fd2cc69166d9215
chenshenhai <cshenhai@gmail.com>
2019-12-11 22:39:34 +0800

    feat: update demo/web_upload

--- demo/web_upload/bodyparser.ts
+++ demo/web_upload/bodyparser.ts
@@ -19,6 +19,33 @@ export interface FormFieldData {
   value: Uint8Array|string;
 }
 
+
+/**
+ * 解析 multipart/form-data 类型表单所有数据
+ * @param {string} boundary 表单数据的分隔符
+ * @param {Uint8Array} stream 表单数据流, 也就是表单HTTP请求体
+ * @return {FormFieldData[]} 表单数据列表
+ *  例如 [{ name: "myName", value: "helloworld" }, { name: "myFile", value: [0,1,...], type: "image/jpeg", filename: "xxx.jpg" }]
+ */
+export async function parseMultipartForm(boundary: string, stream: Uint8Array): Promise<FormFieldData[]> {
+  const fields = await parseMultipartStreamToFields(boundary, stream);
+  const dataList: FormFieldData[] = [];
+  for await (const data of parseMultipartFormField(fields)) {
+    dataList.push(data);
+  }
+  return dataList;
+}
+
+
+/**
+ * 解析 multipart/form-data 类型表单单个数据
+ * 
+ * @param {string} boundary 单个表单数据的分隔符
+ * @param {Uint8Array} stream 单个表单数据流, 也就是表单HTTP请求体
+ * @return {FormFieldData} 单个表单数据
+ *  例如文本类型 { name: "myName", value: "helloworld" }
+ *  例如文件二进制数据 { name: "myFile", value: [0,1,...], type: "image/jpeg", filename: "xxx.jpg" }
+ */
 async function* parseMultipartFormField(fields: Uint8Array[]): AsyncGenerator<FormFieldData> {
   for (let i = 0; i < fields.length; i++) {
     const field = fields[i];
@@ -62,21 +89,21 @@ async function* parseMultipartFormField(fields: Uint8Array[]): AsyncGenerator<Fo
   
 }
 
-export async function parseMultipartForm(boundary: string, stream: Uint8Array): Promise<FormFieldData[]> {
-  const fields = await parseMultipartStreamToFields(boundary, stream);
-  const dataList: FormFieldData[] = [];
-  for await (const data of parseMultipartFormField(fields)) {
-    dataList.push(data);
-  }
-  return dataList;
-}
 
+/**
+ * 将 multipart/form-data 的表单类型按照表单的数据域
+ * 切割成对应的数据缓存区/数据流
+ * 
+ * @param {string} boundary 表单数据的分隔符
+ * @param {Uint8Array} stream 表单数据流, 也就是表单HTTP请求体
+ * @return {Uint8Array[]} 返回表单每个数据的 内存区间/数据流
+ */
 async function parseMultipartStreamToFields(boundary: string, stream: Uint8Array): Promise<Uint8Array[]> {
   const decoder = new TextDecoder();
   const encoder = new TextEncoder();
 
-  const newField = `--${boundary}`;
-  const end = `--${boundary}--`;
+  const newField = `--${boundary}`; // 表单数据分隔符/起始
+  const end = `--${boundary}--`; // 表单终止符
 
   const newFieldChunk = encoder.encode(newField);
   const endChunk = encoder.encode(end);
@@ -89,6 +116,8 @@ async function parseMultipartStreamToFields(boundary: string, stream: Uint8Array
   const fieldOffsetList: FieldChunkOffset[] = [];
   let index: number = 0;
 
+  // 根据分隔符和终止符
+  // 来读取和计算表单内存空间/数据流里每个数据的起始和终止偏移量
   while(!isFinish) {
     const lineChunk = await bufReader.readLineChunk();
     const lineChunkLen = lineChunk.length + CRLF_LEN;
@@ -120,6 +149,8 @@ async function parseMultipartStreamToFields(boundary: string, stream: Uint8Array
     index = endIndex;
   }
 
+  // 根据 表单内存空间/数据流里每个数据的起始和终止偏移量
+  // 切割出每个数据的数据流
   fieldOffsetList.forEach((offset: FieldChunkOffset) => {
     if(offset && offset.start >= 0 && offset.end >= 0) {
       const fieldChunk: Uint8Array = stream.subarray(offset.start, offset.end);
@@ -138,6 +169,7 @@ export interface FormContentType {
 }
 
 /**
+ * 解析 HTTP headers 里 Content-Type的表单类型
  * example: "multipart/form-data; boundary=----WebKitFormBoundaryk7fXm5rwGcU1OJIq"
  * return { enctype: "multipart/form-data", boundary: "----WebKitFormBoundaryk7fXm5rwGcU1OJIq" }
  * 
